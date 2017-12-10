d65499bf4003aea09c7d5f527f1a2be1ffc4d675
jsigee87 <johnsigmon@gmail.com>
2017-12-09 19:19:51 -0600

    added several working models in Build Model. Random Forest performed the best.

--- Kaggle_2017_Titanic/src/setupData.py
+++ Kaggle_2017_Titanic/src/setupData.py
@@ -16,16 +16,6 @@ def load_test_data(data_path = DATA_PATH):
     csv_path = os.path.join(data_path, "test.csv")
     return pd.read_csv(csv_path)
 
-def parseOutAnswers(dataFrame):
-    answers = {}
-
-    for passengerIndex, passenger in dataFrame.iterrows():
-        passengerID = passenger['PassengerId']
-        answers[passengerID] = passenger['Survived']
-
-    return answers
-
-
 ###########################################################
 # 1.) Encode the sex data as 0 for female or 1 for male.
 # 2.) Pull the survived column as answers
@@ -41,7 +31,6 @@ def cleanData(df):
     #This could use a way to detect a column with NaN entries and fill them
     median = df['Age'].median()
     df['Age'].fillna(median, inplace=True)
-    assert(df.isnull()==False)
     
     return df
 
